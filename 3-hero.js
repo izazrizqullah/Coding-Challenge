171c34515ff5a08f7f421e9f1c57ae052c1123b1
izazrizqullah <izazrizqullah@gmail.com>
2024-01-26 11:16:38 +0700

    day 3

---
+++ 3-hero.js
@@ -0,0 +1,22 @@
+// function hero(bullets, dragons) {
+//   if (bullets / 2 >= dragons) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// Cara -2
+
+function hero(bullets, dragons) {
+  const result = bullets / 2 >= dragons ? true : false;
+
+  return result;
+}
+
+console.log(hero(10, 5));
+console.log(hero(7, 4));
+console.log(hero(4, 5));
+console.log(hero(100, 40));
+console.log(hero(1500, 751));
+console.log(hero(0, 1));
